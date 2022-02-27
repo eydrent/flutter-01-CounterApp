b4a737023a20a117d39f4875498a2d971fefad0f
Jose Erney Ospina <joseerneyosp@gmail.com>
2022-02-27 17:30:49 -0500

    ( Chore )[ CounterScreen ] : Refactor widgets

--- lib/screens/counter__screen.dart
+++ lib/screens/counter__screen.dart
@@ -12,6 +12,21 @@ class _CounterScreenState extends State<CounterScreen> {
 
   int counter = 10;
 
+  void increaseCounter() {
+    counter++;
+    setState(() {});
+  }
+
+  void decreaseCounter() {
+    (counter > 0) ? counter-- : null;
+    setState(() {});
+  }
+
+  void resetCounter() {
+    counter = 0;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -32,13 +47,53 @@ class _CounterScreenState extends State<CounterScreen> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          counter++;
-          setState(() {});
-        },
-        child: const Icon(Icons.plus_one_outlined),
+      floatingActionButton: CustomFloatingActions(
+        increaseFn: increaseCounter,
+        decreaseFn: decreaseCounter,
+        resetFn: resetCounter,
       ),
+    );
+  }
+}
+
+class CustomFloatingActions extends StatelessWidget {
+
+  final Function increaseFn;
+  final Function decreaseFn;
+  final Function resetFn;
+
+  const CustomFloatingActions({
+    Key? key,
+    required this.increaseFn,
+    required this.decreaseFn,
+    required this.resetFn,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FloatingActionButton(
+          onPressed: () =>  increaseFn(),
+          // onPressed: () => setState(() => counter++),
+          child: const Icon(Icons.plus_one_outlined),
+        ),
+        const SizedBox(width: 10),
+        FloatingActionButton(
+          backgroundColor: Colors.red,
+          onPressed: () =>  resetFn(),
+          // onPressed: () => setState(() => counter = 0),
+          child: const Icon(Icons.lock_reset_outlined),
+        ),
+        const SizedBox(width: 10),
+        FloatingActionButton(
+          // onPressed: () => setState(() => (counter > 0) ? counter-- : (counter = 0)),
+          child: const Icon(Icons.exposure_minus_1),
+          onPressed: () =>  decreaseFn(),
+        ),
+
+      ],
     );
   }
 }
